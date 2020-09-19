cdc292e24990de22234f934c2303fbdaca25d599
Conner Weatherston <connerweatherston@hotmail.co.uk>
2020-09-19 20:16:57 +0100

    Update VulkanRenderer.cpp

--- ProjectBarnabus/src/GameEngine/VulkanRenderer.cpp
+++ ProjectBarnabus/src/GameEngine/VulkanRenderer.cpp
@@ -56,6 +56,8 @@ bool VulkanRenderer::InitialiseGameEngine()
 	glfwSwapInterval(1);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	return true;
 }
 
 bool VulkanRenderer::ShouldWindowClose()
