f5ce6484ef73b470128ea48bf8aa3f9019cbbcc3
Conner Weatherston <connerweatherston@hotmail.co.uk>
2020-10-10 18:50:32 +0100

    Need to refactor how framebuffer & command buffers are allocated.
    To fix vulkan errors

--- ProjectBarnabus/src/GameEngine/VulkanRenderer.cpp
+++ ProjectBarnabus/src/GameEngine/VulkanRenderer.cpp
@@ -645,7 +645,7 @@ void VulkanRenderer::CreateCommandPool()
 	VkCommandPoolCreateInfo poolInfo{};
 	poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
 	poolInfo.queueFamilyIndex = queueFamilyIndices.graphicsFamily.value();
-	poolInfo.flags = 0; // Optional
+	poolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT; // Optional
 
 	if (vkCreateCommandPool(device, &poolInfo, nullptr, &commandPool) != VK_SUCCESS)
 	{
