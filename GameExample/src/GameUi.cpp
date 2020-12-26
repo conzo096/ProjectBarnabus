726d4a7e136857502cbd28ce9c2711f64732f499
Conner Weatherston <connerweatherston@hotmail.co.uk>
2020-12-26 15:57:56 +0000

    Text is rendered for openGL
    For Vulkan need to do the following:
    1. Implement shader logic
    2. Update text in vertex buffer

--- GameExample/src/GameUi.cpp
+++ GameExample/src/GameUi.cpp
@@ -12,8 +12,9 @@ GameUi::GameUi()
 	// Create initial quad.
 	UiQuad* depthTextureInformation = new UiQuad(glm::vec2(0.6,0.6),glm::vec2(1,1));
 	uiElements.insert(std::pair<std::string, UiQuad*>("depthTexture", depthTextureInformation));
-	//TextQuad* example = new TextQuad(glm::vec2(-1, -1), glm::vec2(1,1));
-	//uiElements.insert(std::pair<std::string, UiQuad*>("example", example));
+	
+	TextQuad* textExample = new TextQuad(glm::vec2(-1, -1), glm::vec2(1,1));
+	uiElements.insert(std::pair<std::string, UiQuad*>("textExample", textExample));
 }
 
 void GameUi::InitGameUi()
@@ -27,26 +28,27 @@ void GameUi::InitGameUi()
 	else
 	{
 		ShaderFactory::CreateShader<VkUIShader>("ui", "res\\Shaders\\Vulkan\\VkUI");
-	//	ShaderFactory::CreateShader<VkFontShader>("font", "res\\Shaders\\Vulkan\\VkFont");
+		ShaderFactory::CreateShader<VkFontShader>("font", "res\\Shaders\\Vulkan\\VkFont");
 	}
 
 	uiElements.at("depthTexture")->GetMeshData().SetShader(BarnabusGameEngine::Get().GetShader("ui"));
-	//uiElements.at("example")->GetMeshData().SetShader(BarnabusGameEngine::Get().GetShader("font"));
+	uiElements.at("textExample")->GetMeshData().SetShader(BarnabusGameEngine::Get().GetShader("font"));
 
 	ITexture* texture = nullptr;
 	if (BarnabusGameEngine::Get().GetRenderType() == IRenderer::OpenGL)
 	{
 		texture = new OpenGLTexture;
 		texture->LoadTexture("res\\textures\\GameFont.png");
+
 		uiElements.at("depthTexture")->GetMeshData().SetTexture(BarnabusGameEngine::Get().GetRenderer()->GetFrameBuffer("main")->GetDepthTexture());
 	}
 	else if (BarnabusGameEngine::Get().GetRenderType() == IRenderer::Vulkan)
 	{
-		//texture = new VulkanTexture;
-		//texture->LoadTexture("res\\textures\\GameFont.png");
+		texture = new VulkanTexture;
+		texture->LoadTexture("res\\textures\\GameFont.png");
 	}
 		
-	//uiElements.at("depthTexture")->GetMeshData().SetTexture(texture);
+	uiElements.at("textExample")->GetMeshData().SetTexture(texture);
 }
 
 void GameUi::SetExampleText(std::string text)
@@ -64,7 +66,7 @@ GameUi::~GameUi()
 
 void GameUi::Draw()
 {
-	//uiElements.at("depthTexture")->GetMeshData().SetTexture(BarnabusGameEngine::Get().GetRenderer()->GetFrameBuffer("main")->GetDepthTexture());
-	//static_cast<TextQuad*>(uiElements.at("example"))->SetText(exampleText);
+	//uiElements.at("textExample")->GetMeshData().SetTexture(BarnabusGameEngine::Get().GetRenderer()->GetFrameBuffer("main")->GetDepthTexture());
+	static_cast<TextQuad*>(uiElements.at("textExample"))->SetText(exampleText);
 	UiDisplay::Draw();
 }
